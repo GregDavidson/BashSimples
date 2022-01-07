0efe640fdfb3b469663311be50986bb7069eee72
GregDavidson <greg.davidson+git@gmail.com>
2022-01-06 23:22:06 -0800

    defensive coding

--- bash_profile.d/fortune.sh
+++ bash_profile.d/fortune.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 # Note: there are a variety of fortune database collections
 # as well as switches to select from those collections
-is_cmd fortune && { echo; fortune; echo; }
+simple_is_cmd fortune && { echo; fortune; echo; }
